adb4f52abac7b1fac91b3ccde3e847865ba3f83d
Coding-Crab <>
2019-06-27 10:20:00 -0400

    change header

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import responseTime from 'response-time';
 import winston from 'winston';
+import cors from 'cors';
 import logger from './lib/logger';
 import settings from './lib/settings';
 import security from './lib/security';
@@ -12,22 +13,19 @@ import dashboardWebSocket from './lib/dashboardWebSocket';
 import ajaxRouter from './ajaxRouter';
 import apiRouter from './apiRouter';
 const app = express();
-
+// app.use(cors());
 security.applyMiddleware(app);
 app.set('trust proxy', 1);
 app.use(helmet());
 app.all('*', (req, res, next) => {
 	// CORS headers
-	res.header(
-		'Access-Control-Allow-Origin',
-		security.getAccessControlAllowOrigin()
-	);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Key, Authorization'
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
+
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	next();
 });
 app.use(responseTime());
